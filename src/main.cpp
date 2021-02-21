d36ac71a7f594f0eed9162955709510d13061547
Martin Šafránek <gismocz@gmail.com>
2021-02-22 00:29:21 +0100

    Reading input

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
 #include <cstring>
+#include <fstream>
 
 using namespace std;
 
 class Grid {
 private:
-    char *data;
+    char *data = nullptr;
     int size;
     int row_len;
-public:
+    string filename;
 
-    Grid(int k) {
+    // PDD specific
+    int k; // přirozené číslo, k>5, reprezentující délku strany šachovnice S o velikosti kxk
+    int q; // přirozené číslo q<k^2/2 reprezentující počet rozmístěných figurek na šachovnici S
+
+public:
+    Grid(const string &filename) {
+        ifstream ifs(filename);
+        ifs >> k;
+        ifs >> q;
         size = k * k;
         row_len = k;
+        this->filename = filename;
         data = new char[size];
+        memset(data, 'X', size);
+
+        char c;
+        char *head = data;
+        while (ifs.get(c) && head < data + size) {
+            if (c != '\n' && c != '\r') {
+                *(head++) = c;
+            }
+        }
+        ifs.close();
     };
 
     Grid(const Grid &oth) {
@@ -24,13 +44,15 @@ public:
     };
 
     ~Grid() {
-        delete[] data;
+        if (data) delete[] data;
     }
 
     friend ostream &operator<<(ostream &os, const Grid &g) {
+        os << g.filename << endl;
+        os << "k=" << g.k << ", q=" << g.q << endl;
         for (int i = 0; i < g.size; i++) {
             os << g.data[i];
-            if (i % g.row_len) os << " | ";
+            if ((i + 1) % g.row_len) os << " | ";
             else os << endl;
         }
         return os << endl;
@@ -39,7 +61,12 @@ public:
 
 };
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv) {
+
+    for (int i = 1; i < argc; i++) {
+        string filename = argv[i];
+        cout << Grid(filename);
+    }
+
     return 0;
 }
